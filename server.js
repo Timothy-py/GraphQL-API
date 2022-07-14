8332ba41718c794f704d92073b4622c32d65943a
Timothy-py <adeyeyetimothy33@gmail.com>
2022-07-14 10:36:51 +0100

    setup express server and GraphQL :rocket:

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql')
+
+const app = express()
+
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: () => "Hello World"
+            }
+        })
+    })
+})
+
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: schema
+}))
+
+app.listen(7000, ()=>{
+    console.log('Server is running on PORT:7000')
+})
