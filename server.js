dd214441b47420e2ee77091138906599a531ea85
Timothy-py <adeyeyetimothy33@gmail.com>
2022-07-18 22:58:37 +0100

    implemented api for adding an author

--- server.js
+++ server.js
@@ -131,6 +131,21 @@ const RootMutationType = new GraphQLObjectType({
                 books.push(book)
                 return book
             }
+        },
+        addAuthor: {
+            type: AuthorType,
+            description: 'Add an author',
+            args: {
+                name: {type: GraphQLNonNull(GraphQLString)}
+            },
+            resolve: (parent, args) => {
+                const author = {
+                    id: books.length + 1,
+                    name: args.name
+                }
+                authors.push(author)
+                return author
+            }
         }
     })
 })
